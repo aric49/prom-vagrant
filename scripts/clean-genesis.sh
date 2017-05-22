e1f10eb92e85e89b326321ab0df7a98ea510745f
Mark Burnett <mark.m.burnett@gmail.com>
2017-05-22 15:20:32 -0500

    be more aggressive with cleanup script

--- scripts/clean-genesis.sh
+++ scripts/clean-genesis.sh
@@ -9,3 +9,8 @@ sudo docker rm -fv $(docker ps -aq)
 sudo rm -rf /etc/kubernetes
 sudo rm -f /etc/systemd/system/kubelet.service
 sudo systemctl daemon-reload
+
+sudo rm -rf /opt/cni
+sudo rm -rf /etc/cni/net.d/*
+sudo rm -rf /var/etcd/*
+sudo rm -f /usr/local/bin/kubelet
