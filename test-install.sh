3a8378226dfa48ac26d5985b40ca67afc6b628b3
Mark Burnett <mark.m.burnett@gmail.com>
2017-05-23 11:27:30 -0500

    move container code to top level

--- test-install.sh
+++ test-install.sh
@@ -5,7 +5,7 @@ set -ex
 # Setup master
 vagrant ssh n0 <<EOS
 set -ex
-sudo docker load -i /vagrant/genesis/promenade-genesis.tar
+sudo docker load -i /vagrant/promenade-genesis.tar
 sudo docker run -v /:/target -v /var/run/docker.sock:/var/run/docker.sock -e NODE_HOSTNAME=n0 quay.io/attcomdev/promenade-genesis:dev
 EOS
 
@@ -13,7 +13,7 @@ EOS
 for node in n1 n2; do
   vagrant ssh $node <<EOS
 set -ex
-sudo docker load -i /vagrant/genesis/promenade-join.tar
+sudo docker load -i /vagrant/promenade-join.tar
 # Should be: sudo docker run -v /:/target -e NODE_HOSTNAME=$node quay.io/attcomdev/promenade-join:dev
 sudo docker run -v /:/target -v /var/run/docker.sock:/var/run/docker.sock -e NODE_HOSTNAME=$node quay.io/attcomdev/promenade-join:dev
 EOS
